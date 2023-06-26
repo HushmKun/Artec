440e0e296304ec49f9b098335b92a55d3ea9fd8b
Hussein Mukhtar <HushmKun@outlook.com>
2023-06-26 20:57:05 +0300

    0.1.4-alpha

---
+++ artec/__main__.py
@@ -0,0 +1,19 @@
+from .argparser import main_args
+from .boiler import boiler_builder
+
+__app_name__ = "Artec"
+__version__ = "0.1.3"
+__desc__ = "Creates a configurable python project \
+    template in a given directory."
+
+
+def main():
+    args = main_args()
+
+    builder = boiler_builder(args.source, args.target,args.verbose)
+
+    builder.build()
+
+
+if __name__ == "__main__":
+    main()
